a874c11f52b01540be23b38e26e241f5a058bae1
Kilativ54 <vitalikechkalov@gmail.com>
2023-05-31 01:35:04 +0200

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formSubmit = document.querySelector(".login-form");
 
+formSubmit.addEventListener("submit", sendForm);
+
+function sendForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть будь ласка всі поля!");
+  }
+  const userProfil = { email: email.value, password: password.value };
+  console.log(userProfil);
+  event.currentTarget.reset();
+}
