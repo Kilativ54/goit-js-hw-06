d618a17bdb4c6c28e7d6f032d2baf9e528c175e6
Kilativ54 <vitalikechkalov@gmail.com>
2023-05-31 02:55:38 +0200

    10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputAmount = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const box = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  const allBoxes = [];
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${30 + 10 * i}px`;
+    div.style.height = `${30 + 10 * i}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    allBoxes.push(div);
+  }
+  return allBoxes;
+};
+const destroyBoxes = () => {
+  box.innerHTML = "";
+}
+btnCreate.addEventListener("click", addBx);
+function addBx(event) {
+  let numberBoxAdd = createBoxes(inputAmount.value);
+  box.append(...numberBoxAdd);
+}
+btnDestroy.addEventListener("click", destr);
+function destr() {
+  destroyBoxes();
+}
